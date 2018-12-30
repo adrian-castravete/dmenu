8fd4788ccbe94bd76ce5cc1e4b074107b1126849
Adrian Castravete <adrian.castravete@gmail.com>
2018-12-30 10:39:13 +0200

    Unified colour & style

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"DejaVu Sans Mono:size=10"
+	"DejaVu Sans:size=12:antialias=true:hinting=true:hintstyle=hintfull"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#dcf5ff", "#508cd7" },
-	[SchemeSel] = { "#55415f", "#dcf5ff" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+		/*       fg         bg       */
+	[SchemeNorm] = { "#55aaff", "#000055" },
+	[SchemeSel]  = { "#ffffff", "#000055" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
