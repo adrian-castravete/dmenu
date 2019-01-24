dec933890424482c2940d8b90ea350fcd71dece4
Adrian Castravete <adrian.castravete@gmail.com>
2019-01-24 14:01:31 +0200

    Font

--- config.def.h
+++ config.def.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"DejaVu Sans:size=12:antialias=true:hinting=true:hintstyle=hintfull"
+	"DejaVu Sans:size=10:weight=bold:antialias=true:hinting=false:hintstyle=hintnone"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
